47a76a675620eaf53cdf979257365eda4cc82f8f
Aaron Bannert <aaron@apache.org>
2001-07-31 02:12:50 +0000

    Doh! I spent waaaay to long on this one. This patch makes the #define double-include symbol the same as the filename. (I reused the symbol and couldn't figure out why the heck it wasn't including my new file of the same name!)
    git-svn-id: https://svn.apache.org/repos/asf/httpd/test/trunk/flood@89813 13f79535-47bb-0310-9956-ffa450edef68

--- flood_net_ssl.h
+++ flood_net_ssl.h
@@ -54,8 +54,8 @@
  * Originally developed by Aaron Bannert and Justin Erenkrantz, eBuilt.
  */
 
-#ifndef __flood_socket_ssl_h
-#define __flood_socket_ssl_h
+#ifndef __flood_net_ssl_h
+#define __flood_net_ssl_h
 
 #include <apr_network_io.h> /* apr_socket_t */
 #include <apr_pools.h>      /* apr_pool_t */
@@ -68,4 +68,4 @@ void ssl_close_socket(ssl_socket_t *s);
 apr_status_t ssl_write_socket(ssl_socket_t *s, request_t *r);
 apr_status_t ssl_read_socket(ssl_socket_t *s, char *buf, int *buflen);
 
-#endif  /* __flood_socket_h */
+#endif  /* __flood_net_socket_h */
