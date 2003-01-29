4d6c526d38fefc4ea00025225dbddfeacb3208a5
Justin Erenkrantz <jerenkrantz@apache.org>
2003-01-29 06:13:27 +0000

    Added error handling for failed config-file open.
    This patch adds error handling to the code that opens the
    configuration file.  When you specify an argument to the flood
    program, it attempts to open it to read in configuration
    information.  Previously, there was no error handling, so
    the code proceeded to use an invalid file handle when the
    file open fails.  The code now detects the failure and displays
    an error message.
    
    The standard error and output file-opens were moved earlier
    in the program in case we need to display error messages
    to standard error.
    
    On Windows, using the invalid file handle results in an
    access violation.  The program now displays a message like
    this:
      Error opening configuration file: The system cannot find
      the file specified.  .
    
    Submitted by: Phi-Long Tran <ptran@pobox.com>
    Reviewed by: Justin Erenkrantz (minor formatting tweak)
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/test/trunk/flood@98527 13f79535-47bb-0310-9956-ffa450edef68

--- flood.c
+++ flood.c
@@ -144,16 +144,22 @@ int main(int argc, char** argv)
     ssl_init_socket(local_pool);
 #endif /* FLOOD_HAS_OPENSSL */
    
-    if (argc == 1)
-        apr_file_open_stdin(&local_stdin, local_pool);
-    else
-    {
-        apr_file_open(&local_stdin, argv[1], APR_READ, APR_OS_DEFAULT, 
-                      local_pool);
-    }
-
     apr_file_open_stdout(&local_stdout, local_pool);
     apr_file_open_stderr(&local_stderr, local_pool);
+
+    if (argc == 1) {
+        apr_file_open_stdin(&local_stdin, local_pool);
+    }
+    else if ((stat = apr_file_open(&local_stdin, argv[1], APR_READ,
+                         APR_OS_DEFAULT, local_pool)) != APR_SUCCESS)
+    {
+        char buf[256];
+        apr_strerror(stat, (char*) &buf, sizeof(buf));
+        apr_file_printf(local_stderr,
+                        "Error opening configuration file: %s.\n",
+                        (char*)&buf);
+        exit(-1);
+    }
 
     /* parse the config */
     config = parse_config(local_stdin, local_pool);
